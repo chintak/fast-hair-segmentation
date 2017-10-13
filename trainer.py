cae375cc638e32cd3857a98a0e258223a5dff52c
chintak <chintaksheth@gmail.com>
2017-10-13 11:48:48 -0400

    test func

--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import numpy as np
 import os
 from skimage.io import imread
+from sklearn.datasets import load_svmlight_file
 import xgboost as xgb
 
 import data
@@ -24,8 +25,26 @@ def train(model_fname, model_type, dtrain, dval=None, dtest=None, cont=None):
     bst.dump_model('dump.raw.txt')
 
 
-def test():
+def test(model_fname, model_type, fset):
+    params = getattr(configs, model_type)(val=True)
+    bst = xgb.Booster(params=params)
+    bst.load_model(model_fname)
+    nset = load_svmlight_file(fset)
+    dset = xgb.DMatrix(nset)
+    preds = bst.predict(dset)
     pass
+
+
+def get_model_feature_type(name):
+    n, e = os.path.splitext(os.path.basename(name))
+    mtype, ftype = n.split('_')[:2]
+    if not hasattr(configs, mtype):
+        print 'Invalid model type in {}. See configs.py'.format(name)
+        mtype, ftype = None, None
+    if not hasattr(configs, ftype):
+        print 'Invalid feature type in {}. See configs.py'.format(name)
+        mtype, ftype = None, None
+    return mtype, ftype
 
 
 def args():
@@ -41,8 +60,7 @@ def args():
 
 if __name__ == '__main__':
     parse = args()
-    n, e = os.path.splitext(os.path.basename(parse.model))
-    mtype = n.split('_')[0]
+    mtype, ftype = get_model_feature_type(parse.model)
     if not hasattr(configs, mtype):
         import sys
         print 'Invalid model type. See configs.py'
@@ -57,4 +75,4 @@ if __name__ == '__main__':
               dtrain, dval=dval, dtest=dtest,
               cont=parse.cont)
     elif dtest or dval:
-        test()
+        test(parse.model, mtype, dval=dval, dtest=dtest)
